fa4c62528a1074b20b2c5950e379ec622db2ec1a
masteryi <1796340674@qq.com>
2025-11-16 12:48:00 +0800

    初始化仓库

---
+++ src/main/java/com/yi/musiclisten/MusicListenApplication.java
@@ -0,0 +1,13 @@
+package com.yi.musiclisten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicListenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicListenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yi/musiclisten/MusicListenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yi.musiclisten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicListenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
